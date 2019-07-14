922ddface581331c1d2651279506eee9a765d381
dkasri <dannykasri@gmail.com>
2019-07-14 02:21:25 -0700

    Initial Commit

---
+++ LearnMoneyApp/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  LearnMoneyApp
+//
+//  Created by Danny Kasri on 7/14/19.
+//  Copyright © 2019 Danny Kasri. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ LearnMoneyApp/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  LearnMoneyApp
+//
+//  Created by Danny Kasri on 7/14/19.
+//  Copyright © 2019 Danny Kasri. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
